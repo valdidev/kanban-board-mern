bd2886c298769e74b5a9ef0c64e1e56c1faedfef
valdidev <iamvaldidev@gmail.com>
2022-12-23 18:05:05 +0100

    wip: login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,40 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { Box, TextField } from "@material-ui/core";
+import { useState } from "react";
+import { LoadingButton } from "@mui/lab";
 
-export default Login
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => null;
+
+  return (
+    <>
+      <Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+        />
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          type="password"
+          disabled={loading}
+        />
+        <LoadingButton>
+          Login
+        </LoadingButton>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
