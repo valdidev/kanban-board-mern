12bde9c97d0156165db378faaa40442915da837f
valdidev <iamvaldidev@gmail.com>
2022-12-23 16:17:07 +0100

    fix: warning mongoose strict query

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
+const cors = require('cors')
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false)
 
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
