3495f96d701459cf13f8960557939092e053c817
valdidev <iamvaldidev@gmail.com>
2022-12-23 17:35:43 +0100

    feat: authlayout done

--- client/src/layout/AuthLayout.jsx
+++ client/src/layout/AuthLayout.jsx
@@ -1,19 +1,47 @@
 import { useEffect, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import authUtils from "../utils/authUtils";
+import Loading from "../common/Loading";
+import { Box, Container } from "@mui/system";
+import assets from "../assets";
 
 const AuthLayout = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [loading, setLoading] = useState(true);
 
-  /* useEffect(() => {
-    
-  }, []) */
+  useEffect(() => {
+    const checkAuth = async () => {
+      const isAuth = await authUtils.isAuthenticated();
+      if (!isAuth) {
+        setLoading(false);
+      } else {
+        navigate("/");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
 
-  return (
-    <div>
-      <Outlet />
-    </div>
+  return loading ? (
+    <Loading fullHeight />
+  ) : (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <img
+          src={assets.images.logoDark}
+          alt="app logo"
+          style={{ width: "100px" }}
+        />
+        <Outlet />
+      </Box>
+    </Container>
   );
 };
 
