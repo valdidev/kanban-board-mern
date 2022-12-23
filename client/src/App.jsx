83086d8e6387eae5ddb1b32f7fd02afc277f0f73
valdidev <iamvaldidev@gmail.com>
2022-12-23 16:39:25 +0100

    chore: app imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import CssBaseLine from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import AuthLayout from "./layout/AuthLayout";
+import Home from "./pages/Home";
+import Board from "./pages/Board";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
+  const theme = createTheme({
+    palette: { mode: "dark" },
+  });
+
   return (
-    <div>
-      kanban
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseLine>
+      </CssBaseLine>
+    </ThemeProvider>
   );
 }
 
