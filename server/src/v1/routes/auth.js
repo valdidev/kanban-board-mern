d2332875186daa39a2f2a567a01aad8a2c295c55
valdidev <iamvaldidev@gmail.com>
2022-12-23 14:04:43 +0100

    wip: route auth

---
+++ server/src/v1/routes/auth.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const userController = require('../controllers/userController');
+const { body } = require('express-validator');
+const validation = require('../handlers/validation');
+const tokenHandler = require('../handlers/tokenHandler');
+const User = require('../models/user');
+
+router.post(
+    '/signup', 
+    body('username').isLength({min: 8}).withMessage(
+        'username must be at least 8 characters'
+    ),
+    body('password').isLength({min: 8}).withMessage(
+        'password must be at least 8 characters'
+    ),
+    body('confirmPassword').isLength({min: 8}).withMessage(
+        'confirmPassword must be at least 8 characters'
+    ),
+    body('username').custom(value => {
+        return User.findOne({username: value}).then(user => {
+            if (user) {
+                return Promise.reject('username already exist');
+            }
+        })
+    }),
+)
