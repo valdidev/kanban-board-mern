0265d44e1a81f4ff2fb8c8069a57798a97100151
valdidev <iamvaldidev@gmail.com>
2022-12-23 13:22:59 +0100

    feat: user controller: login

--- server/src/v1/controllers/user.js
+++ server/src/v1/controllers/user.js
@@ -12,12 +12,56 @@ exports.register = async (req, res) => {
         );
 
         const user = await User.create(req.body);
+
         const token = jsonwebtoken.sign(
             { id: user._id },
             { expiresIn: '24h' }
         );
-        res.status(200).json({user, token});
+
+        res.status(200).json({ user, token });
     } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username }).select('password username');
+        if (!user) {
+            return res.status(401).json({
+                errors: [
+                    {
+                        param: 'username',
+                        msg: 'Invalid username or password'
+                    }
+                ]
+            });
+        };
+
+        const decryptedPass = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASSWORD_SECRET_KEY
+        ).toString(CryptoJS.enc.Utf8);
+
+        if (decryptedPass !== password) {
+            return res.status(401).json({
+                errors: [
+                    {
+                        param: 'username',
+                        msg: 'Invalid username or password'
+                    }
+                ]
+            });
+        };
+        const token = jsonwebtoken.sign(
+            { id: user._id },
+            { expiresIn: '24h' }
+        );
+
+        res.status(200).json({ user, token });
+    } catch (error) {
         res.status(500).json(err);
     }
 };
