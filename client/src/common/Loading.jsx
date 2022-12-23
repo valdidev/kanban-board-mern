5e000d803694a630501d1b6a12bf457970300d23
valdidev <iamvaldidev@gmail.com>
2022-12-23 16:30:53 +0100

    feat: loading component

--- client/src/common/Loading.jsx
+++ client/src/common/Loading.jsx
@@ -1,8 +1,19 @@
+import { Box, CircularProgress } from '@mui/material';
 
-const Loading = () => {
+const Loading = props => {
   return (
-    <div>Loading</div>
-  )
-}
+    <Box sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: props.fullHeight ? '100vh' : '100%'
+    }}>
 
-export default Loading
+      <CircularProgress />
+
+    </Box>
+  )
+};
+
+export default Loading;
