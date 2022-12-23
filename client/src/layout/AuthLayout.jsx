a77161b8dcff5837e0ec4cf62dadef200e35b2de
valdidev <iamvaldidev@gmail.com>
2022-12-23 17:21:05 +0100

    feat: authUtils done

--- client/src/layout/AuthLayout.jsx
+++ client/src/layout/AuthLayout.jsx
@@ -1,6 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const AuthLayout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  /* useEffect(() => {
+    
+  }, []) */
+
   return (
     <div>
       <Outlet />
